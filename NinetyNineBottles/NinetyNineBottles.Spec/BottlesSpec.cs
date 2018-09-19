c8b61cfb76ad26093ffb8dd9b9b44821f062c7ee
John Nicholas <themrtortoise@gmail.com>
2018-09-19 21:30:26 +0100

    added a few verses

--- NinetyNineBottles/NinetyNineBottles.Spec/BottlesSpec.cs
+++ NinetyNineBottles/NinetyNineBottles.Spec/BottlesSpec.cs
@@ -69,5 +69,22 @@ Take one down and pass it around, 97 bottles of beer on the wall.
 VERSES";
             Assert.Equal(expected, new Bottles().Verses(99,98));
         }
+
+        [Fact]
+        public void A_few_verses()
+        {
+            var expected = @"VERSES
+2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+
+1 bottle of beer on the wall, 1 bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.
+
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+VERSES";
+            
+            Assert.Equal(expected, new Bottles().Verses(2,0));
+        }
     }
 }
